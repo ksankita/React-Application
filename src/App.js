0f29952cebba0df92920301e1a96fa98c9e077fb
ksankita <129387493+ksankita@users.noreply.github.com>
2023-07-26 23:12:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,73 @@
+
+import './App.css';
+import React,{ Component} from 'react';
+import axios from 'axios';
+
+class App extends Component {
+  state = {
+    selectedFile: null,
+    fileUploadedSuccessfully: false
+  }
+  onFileChange = event => {
+    this.setState({selectedFile: event.target.files[0]});
+  }
+  onFileUpload = () => {
+    const formData =new FormData();
+    formData.append(
+      "demo file",
+      this.state.selectedFile,
+      this.state.selectedFile.name
+    )
+    axios.post("https://idj1gltime.execute-api.ap-south-1.amazonaws.com/prod/file-upload",formData).then(() => {
+      this.setState({selectedFile: null});
+      this.setState({fileUploadedSuccessfully:true});
+    })
+  
+  }
+
+  fileData =() => {
+    if (this.state.selectedFile){
+      return (
+      <div>
+        <h2>File Details:</h2>
+        <p>File name:{this.state.selectedFile.name}</p>
+        <p>File Type:{this.state.selectedFile.type}</p>
+        <p>Last Modified: {" "}
+           {this.state.selectedFile.lastModifiedDate.toDateString()}
+        </p>
+      </div>
+      );
+    }
+    else if (this.state.fileUploadedSuccessfully){
+      return (
+        <div>
+          <br />
+          <h4>Your File Uploaded Successfully</h4>
+        </div>
+      );
+    }
+    else{
+      return (
+        <div>
+          <br />
+          <h4>Choose a file and then press the upload button</h4>
+        </div>
+      )
+    }
+  }
+  render(){
+    return(
+      <div className='container'>
+        <h2>File Upload Sysem</h2>
+        <div>
+          <input type="file" onChange={this.onFileChange} />
+          <button onClick={this.onFileUpload}>
+            Upload
+          </button>
+        </div>
+        {this.fileData()}
+      </div>
+    )
+  }
+}
+export default App;
